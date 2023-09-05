7aedb60a9ceb46e2d574b4013d269b7c0160af1e
Jayboi visuals <114653679+agananojoshua@users.noreply.github.com>
2023-09-06 00:25:47 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard.jsx";
+import './App.css'
+// import SearchIcon from './search.svg';
+import SearchIcon from './search.svg';
+
+// 30af137b
+
+const API_URL = 'http://www.omdbapi.com?apikey=30af137b';
+
+const movie1 = {
+    Title: 'Italian Spiderman', 
+    Year: '2007', 
+    imdbID: 'tt2705436', 
+    Type: 'movie', 
+    Poster: 'N/A'
+}
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>Jay_Movies</h1>
+
+            <div className="search">
+                <input 
+                placeholder="Search for movies" 
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img src={SearchIcon} alt="search"
+                onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {movies?.length > 0
+                ? (
+                    <div className="container">
+                        {movies.map((movie) =>(
+                            <MovieCard movie={movie} />
+                        ))
+
+                        }
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No Movies Found</h2>
+                    </div>
+                )}
+        </div>
+    );
+}
+
+export default App;
